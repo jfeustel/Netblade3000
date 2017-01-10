ff27cab18e727ebfff32e83ea838453bb513651e
J Feustel <jfeustel@gmail.com>
2017-01-10 14:23:26 -0600

    Left Room
    Left room updated with gun sequence.

--- Program.cs
+++ Program.cs
@@ -2,30 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Netblade3000
 {
     class Program
     {
+        public bool hasKey = false;
+        public bool hasGun = false;
+
         static void Main(string[] args)
         {
+
             Console.WriteLine("WELCOME USER");
             Console.WriteLine("You see a door.");
             Console.WriteLine("Enter door? y/n");
             String enter1 = Console.ReadLine();
             if (yesNo(enter1))
             {
+                Console.WriteLine("");
                 room1();
             }
             else
             {
+                Console.WriteLine("");
                 Console.WriteLine("There is no other way to go.");
-                Console.WriteLine("Enter door? y/n");
-                if (yesNo(enter1))
-                {
-                    room1();
-                }
+                Console.WriteLine("You begrudgingly open the door...");
+                room1();
             }
             
         }
@@ -42,16 +46,15 @@ namespace Netblade3000
             String enter2 = Console.ReadLine();
             if (yesNo(enter2))
             {
+                Console.WriteLine("");
                 hall1();
             }
             else
             {
                 Console.WriteLine("There is no other way to go.");
-                Console.WriteLine("Enter door? y/n");
-                if (yesNo(enter2))
-                {
-                    hall1();
-                }
+                Console.WriteLine("You begrudgingly open the door...");
+                Console.WriteLine("");
+                hall1();
             }
         }
 
@@ -67,31 +70,68 @@ namespace Netblade3000
             String enter3 = Console.ReadLine();
             if (leftRightStraight(enter3) == 'l')
             {
+                Console.WriteLine("");
                 room2();
             }
-            if (leftRightStraight(enter3) == 'l')
+            if (leftRightStraight(enter3) == 'r')
             {
+                Console.WriteLine("");
                 room3();
             }
-            if (leftRightStraight(enter3) == 'l')
+            if (leftRightStraight(enter3) == 's')
             {
+                Console.WriteLine("");
                 room4();
             }
             if (leftRightStraight(enter3) == 'n')
             {
                 Console.WriteLine("INVALID ENTRY.");
+                Console.WriteLine("");
                 hall1();                   
             }
         }
 
         public static void room2()
         {
-            Console.WriteLine("Bad guy, key.");
+            Console.WriteLine("A guard sleeps in front of security cam screens.");
+            Console.WriteLine("You see a radgun clipped to his belt.");
+            Console.WriteLine("Try to take the radgun? y/n");
+            String stealGun = Console.ReadLine();
+            if (yesNo(stealGun))
+            {
+                Console.WriteLine("You reach to unclip the radgun from the guard's belt.");
+                Console.WriteLine("The guard snorts and turns in his chair");
+                Thread.Sleep(2000);
+                Console.WriteLine("He is still asleep. You quickly unclip the radgun and slide it into your coat.");
+                Thread.Sleep(1000);
+                Console.WriteLine("RECEIVED ITEM - RADGUN V3.5");
+                //hasGun = true;
+                Thread.Sleep(2000);
+                Console.WriteLine("Leave room?");
+                String leaveRoom = Console.ReadLine();
+                if (yesNo(leaveRoom))
+                {
+                    hall1();
+                }
+                else
+                {
+                    Console.WriteLine("You should leave before he wakes up...");
+                    Thread.Sleep(2000);
+                    hall1();
+                }
+            }
+            else
+            {
+                Console.WriteLine("You slowly back out of the room...");
+                Thread.Sleep(2000);
+                hall1();
+            }
+
         }
 
         public static void room3()
         {
-            Console.WriteLine("Bad guy, treasure");
+            Console.WriteLine("Bad guy, key");
         }
 
         public static void room4()
@@ -109,7 +149,6 @@ namespace Netblade3000
 
             else if (yn == "N" || yn == "n" || yn == "no" || yn == "No")
             {
-                Console.WriteLine("YOU DIE");
                 return false;
             }
             else
